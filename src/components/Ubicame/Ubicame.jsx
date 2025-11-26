feb295953aec193feced703c05bc00ca6fba9f98
yorliana <ing.yorlianam24@gmail.com>
2025-11-27 00:20:14 +0100

    Contacto icons

--- src/components/Ubicame/Ubicame.jsx
+++ src/components/Ubicame/Ubicame.jsx
@@ -1,48 +1,65 @@
 import React from "react";
-import styles from './Ubicame.module.css'
+import styles from "./Ubicame.module.css"
 import img22 from "../../assets/contacto/img22.webp"
-import img23 from "../../assets/contacto/img23.webp"
-import img24 from "../../assets/contacto/img24.webp"
-import img from "../../assets/contacto/Animacion.gif"
-import img25 from '../../assets/contacto/img25.webp'
-import img26 from '../../assets/contacto/img26.webp'
-import img27 from '../../assets/contacto/img27.webp'
-import img28 from '../../assets/contacto/img28.png'
+import img23 from "../../assets/contacto/img23.webp";
+import img25 from "../../assets/contacto/img23.webp";
+import img26 from "../../assets/contacto/img26.webp";
+import img27 from "../../assets/contacto/img27.webp";
+import img28 from "../../assets/contacto/img28.png";
+import imgAnim from "../../assets/contacto/Animacion.gif";
 
 export default function Ubicame() {
   return (
-    <div className={styles.contenedor}>
-      
-      {/* GIF centrado */}
-      <img className={styles.img} src={img} alt="Animación" />
+    <>
+    
+  <div className={styles.contenedor}>
 
-      {/* Botones arriba */}
-      <div className={styles.botonesContainer}>
-        <a href="https://github.com/yorliana" target="_blank" className={styles.boton}>
-          <img src={img22} alt="icono 1" />
-        </a>
+  {/* CONTENEDOR DEL GIF + ICONOS */}
+  <div className={styles.areaCentral}>
+
+    {/* GIF detrás */}
+    <img className={styles.gifDetras} src={imgAnim} alt="Animación" />
+
+    {/* GRID de íconos */}
+    <div className={styles.grid}>
+      
+      <a href="https://github.com/yorliana" target="_blank" className={styles.caja}>
+        <img src={img22} alt="GitHub" />
+      </a>
+
+      <div className={styles.cajaDecoracion}>
         <img src={img25} alt="" />
       </div>
 
-      {/* Botones abajo */}
-      <div className={styles.botonesContainer}>
-        <a href="https://www.linkedin.com/in/yorlianamu%C3%B1oz/" target="_blank" className={styles.boton}>
-          <img src={img23} alt="icono 2" />
-        </a>
-        <a href="https://whassapt" target="_blank" className={styles.boton}>
-          <img className={styles.img28} src={img28} alt="icono 3" />
-        </a>
-        <a href="https://linkedin.com" target="_blank" className={styles.boton}>
-          <img src={img24} alt="icono 4" />
-        </a>
+      <a href="https://www.linkedin.com/in/yorlianamu%C3%B1oz/" target="_blank" className={styles.caja}>
+        <img src={img23} alt="LinkedIn" />
+      </a>
+
+      <a href="https://whatsapp.com" target="_blank" className={styles.caja}>
+        <img src={img28} alt="WhatsApp" />
+      </a>
+
+      <a href="https://gmail.com" target="_blank" className={styles.caja}>
+        <img src={img25} alt="Gmail" />
+      </a>
+
+      <div className={styles.cajaDecoracion}>
         <img src={img26} alt="" />
       </div>
 
-      {/* Imagen adicional */}
-      <div>
-        <img src={img27} alt="" />
-      </div>
-
     </div>
+
+  </div>
+
+  <img className={styles.imgBottom} src={img27} alt="" />
+
+</div>
+
+   
+   </>
   );
 };
+
+
+
+
