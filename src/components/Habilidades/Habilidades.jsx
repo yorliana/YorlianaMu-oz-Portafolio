6e1923ebf2810b8943f0ef5994305fcaa6fbf8dd
yorliana <ing.yorlianam24@gmail.com>
2025-12-02 00:09:52 +0100

    skrills actualizadas

--- src/components/Habilidades/Habilidades.jsx
+++ src/components/Habilidades/Habilidades.jsx
@@ -1,25 +1,32 @@
 import React from "react"; 
 import styles from './Habilidades.module.css'
 import heroImage from "../../assets/habilidades/img13.webp"
+import img15 from '../../assets/habilidades/img15.webp'
+import img16 from '../../assets/habilidades/img16.png'
+import img14 from '../../assets/habilidades/img14.webp'
 
 export default function Habilidades() {
 
   return (
   
-    <div className={styles.contenido}>
-      
-     <div className= {styles.linea}>
-      
-      </div>
-
-      <div className={styles.wrapper}>
-        
-        {/* Imagen */}
-        <div className={styles.imageContainer}>
+    <div className={styles.container}>
+      <div className={styles.heroImage}>
+        <img className={styles.cuadro} src={img15} alt="" />
           <img src={heroImage} alt="perfil" />
+          <img className={styles.y} src={img14}  alt="" />
         </div>
-
-        {/* Texto */}
+    <div className= {styles.linea}>
+      <hr classname={styles.linea}/>
+     <div className={styles.h1}>
+      <h1 className={styles.h1}>Multitask Comprometida Creativa </h1></div>
+<img  className={styles.cuadros} src={img16}alt="" />
+      <hr classname={styles.linea}/>
+        
+    
+     
+      <div className={styles.wrapper}>
+   
+    
         <div className={styles.textContainer}>
           <p className={styles.letras}>
             Html, Css, Js, React, <br />
@@ -32,6 +39,13 @@ export default function Habilidades() {
           </p>
         </div>
 
+
+
+      </div> 
+      
+     
+
+      
       </div>
 
     
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,44 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./NavBar.module.css";
 import logo from "../../assets/landing/img3.webp";
 import flecha from "../../assets/perfil/imgc.webp";
 import { Link } from "react-router-dom";
+
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <header className={styles.navbar}>
-      <Link to="/">
-       <img className={styles.logo} src={logo} alt="logo" />
-     </Link>
+      <Link to="/" className={styles.logoLink}>
+        <img className={styles.logo} src={logo} alt="logo" />
+      </Link>
 
+      {/* Menú escritorio */}
       <nav className={styles.nav}>
-        <Link to="/proyectos" className={styles.a} >
+        <Link to="/proyectos" className={styles.a}>
           PROYECTOS
           <span className={styles.icon}>
-            <img src={flecha} alt="icono" />
+            <img src={flecha} alt="flecha" />
           </span>
         </Link>
 
-        <Link to="/conoceme" className={styles.a}>CONÓCEME
+        <Link to="/conoceme" className={styles.a}>
+          CONÓCEME
           <span className={styles.icon}>
-            <img src={flecha} alt="icono" />
+            <img src={flecha} alt="flecha" />
           </span>
         </Link>
 
         <Link to="/skrills" className={styles.a}>
           SKILLS
           <span className={styles.icon}>
-            <img src={flecha} alt="icono"  />
+            <img src={flecha} alt="flecha" />
           </span>
         </Link>
 
         <Link to="/contacto" className={styles.a}>
           CONTACTO
           <span className={styles.icon}>
-            <img src={flecha} alt="icono" />
+            <img src={flecha} alt="flecha" />
           </span>
         </Link>
       </nav>
-    
+
+      {/* Botón hamburguesa (móvil) */}
+      <button
+        className={styles.menuBtn}
+        onClick={() => setOpen(!open)}
+        aria-label={open ? "Cerrar menú" : "Abrir menú"}
+      >
+        {open ? "✕" : "☰"}
+      </button>
+
+      {/* Menú móvil */}
+      {open && (
+        <div className={styles.mobileNav} role="menu" aria-hidden={!open}>
+          <Link to="/proyectos" className={styles.mobileItem} onClick={() => setOpen(false)}>
+            PROYECTOS
+            <span className={styles.icon}>
+              <img src={flecha} alt="flecha" />
+            </span>
+          </Link>
+
+          <Link to="/conoceme" className={styles.mobileItem} onClick={() => setOpen(false)}>
+            CONÓCEME
+            <span className={styles.icon}>
+              <img src={flecha} alt="flecha" />
+            </span>
+          </Link>
+
+          <Link to="/skrills" className={styles.mobileItem} onClick={() => setOpen(false)}>
+            SKILLS
+            <span className={styles.icon}>
+              <img src={flecha} alt="flecha" />
+            </span>
+          </Link>
+
+          <Link to="/contacto" className={styles.mobileItem} onClick={() => setOpen(false)}>
+            CONTACTO
+            <span className={styles.icon}>
+              <img src={flecha} alt="flecha" />
+            </span>
+          </Link>
+        </div>
+      )}
     </header>
   );
 };
